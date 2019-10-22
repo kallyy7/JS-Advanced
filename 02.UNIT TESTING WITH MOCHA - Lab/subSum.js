2987d6e6b441cc2b902e28cd6d410b15bb137cca
kallyy7 <milanova1203@gmail.com>
2019-10-22 18:28:02 +0300

    .

--- 02.UNIT TESTING WITH MOCHA - Lab/subSum.js
+++ 02.UNIT TESTING WITH MOCHA - Lab/subSum.js
@@ -1,4 +1,6 @@
-function solve(arr, firstIndex, secIndex) {
+const assert = require('chai').assert;
+
+function subSum(arr, firstIndex, secIndex) {
     if (!Array.isArray(arr)) {
         return NaN;
     }
@@ -15,7 +17,7 @@ function solve(arr, firstIndex, secIndex) {
         secIndex = arr.length;
     }
 
-    if (!arr.every((i) => i instanceof Number)) {
+    if (!arr.every(Number)) {
         return NaN;
     }
 
@@ -25,6 +27,20 @@ function solve(arr, firstIndex, secIndex) {
         .reduce((a, b) => a + b);
 }
 
-solve([10, 20, 30, 40, 50, 60], 3, 300)
-console.log(solve([2, 3, "test"], -2, 100));
-console.log(solve([], -2, 100));
+subSum([10, 20, 30, 40, 50, 60], 3, 300)
+console.log(subSum([2, 3, "test"], -2, 100));
+console.log(subSum([], -2, 100));
+
+// test with mocha and chai
+describe('subSum', function () {
+    it('test', function () {
+        // Arrange
+        let arr = [];
+        let firstIndex = -2;
+        let secIndex = 100;
+        // Act
+        let result = subSum(arr, firstIndex, secIndex);
+        //Assert
+        assert.equal(result, 0);
+    });
+})
